8933757b6b6651724efed55426c36461e177f0d9
acisdude <acisdude@cfa.harvard.edu>
2018-01-02 17:21:22 -0500

    Bugfixes related to the location of the templates

--- acisfp_check/acisfp_check.py
+++ acisfp_check/acisfp_check.py
@@ -326,7 +326,7 @@ class ACISFPCheck(ACISThermalCheck):
                    'fp_sens_viols': pred["fp_sens_viols"],
                    'cti_viols': pred["cti_viols"]}
 
-        template_path = os.path.join(model_path, '..', 'templates')
+        template_path = os.path.join(model_path, 'templates')
 
         self.write_index_rst(self.bsdir, args.outdir, context, 
                              template_path=template_path)
